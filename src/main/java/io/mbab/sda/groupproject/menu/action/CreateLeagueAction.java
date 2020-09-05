00a7fa30edb8224e9584aab23764c982fc030104
Uzytkownik <uzytkownik@kursGIT.pl>
2020-09-05 14:28:51 +0200

    create CreateTeamAction and LeagueRepository, and waiting for Countryrepository

---
+++ src/main/java/io/mbab/sda/groupproject/menu/action/CreateLeagueAction.java
@@ -0,0 +1,15 @@
+package io.mbab.sda.groupproject.menu.action;
+
+public class CreateLeagueAction implements MenuAction {
+
+
+    @Override
+    public void execute() {
+
+
+        System.out.println("0) Przejdź do poprzedniego menu");
+        System.out.println("Podaj nazwę ligi:");
+
+
+    }
+}
--- src/main/java/io/mbab/sda/groupproject/menu/action/CreateTeamAction.java
+++ src/main/java/io/mbab/sda/groupproject/menu/action/CreateTeamAction.java
@@ -6,39 +6,34 @@ import io.mbab.sda.groupproject.menu.CustomScanner;
 import io.mbab.sda.groupproject.repository.LeagueRepository;
 import lombok.RequiredArgsConstructor;
 
-
-
 @RequiredArgsConstructor
 public class CreateTeamAction implements MenuAction {
 
-    CustomScanner cs;
-    LeagueRepository leagueRepository;
-//    CountryRepository countryRepository;
+  private final CustomScanner cs;
+  LeagueRepository leagueRepository;
+  //    CountryRepository countryRepository;
 
-
-    @Override
-    public void execute() {
+  @Override
+  public void execute() {
 
     System.out.println("--> Wciśnięcie '0' powoduję powtór do menu głównego <--");
     System.out.println("Podaj nazwę drużyny:");
 
+    String name = cs.nextLine();
+    var builder = Team.builder().name(name);
 
-        String name = cs.nextLine();
-        var builder = Team.builder().name(name);
+    System.out.println("Podaj nawę Ligi:");
+    String leagueName = cs.nextLine();
 
-        System.out.println("Podaj nawę Ligi:");
-        String leagueName = cs.nextLine();
-
-        League league = leagueRepository.findByName(leagueName);
-        if(league == null){
+    League league = leagueRepository.findByName(leagueName);
+    if (league == null) {
       System.out.println("Tworzysz nową lige o nazwie " + leagueName);
       System.out.println("Z jakiego Państwa jest ta liga?");
       String country = cs.nextLine();
 
-
-        }else {
-            builder.league(league);
-        }
+    } else {
+      builder.league(league);
+    }
 
     System.out.println("Podaj nawę miasta:");
     String city = cs.nextLine();
@@ -47,6 +42,5 @@ public class CreateTeamAction implements MenuAction {
     System.out.println("Podaj wartość drużyny");
     double value = Double.parseDouble(cs.nextLine());
     builder.city(city);
-    }
-
+  }
 }

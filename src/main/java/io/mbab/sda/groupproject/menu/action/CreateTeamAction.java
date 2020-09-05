6d57ed14286ffdfe0c4fb969c9b8176308129261
jasmistrz <64699579+jasmistrz@users.noreply.github.com>
2020-09-05 14:46:01 +0200

    create CreateTeamAction and LeagueRepository, and waiting for Country… (#5)
    * create CreateTeamAction and LeagueRepository, and waiting for Countryrepository
    
    Co-authored-by: Uzytkownik <uzytkownik@kursGIT.pl>

---
+++ src/main/java/io/mbab/sda/groupproject/menu/action/CreateTeamAction.java
@@ -0,0 +1,46 @@
+package io.mbab.sda.groupproject.menu.action;
+
+import io.mbab.sda.groupproject.entity.League;
+import io.mbab.sda.groupproject.entity.Team;
+import io.mbab.sda.groupproject.menu.CustomScanner;
+import io.mbab.sda.groupproject.repository.LeagueRepository;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class CreateTeamAction implements MenuAction {
+
+  private final CustomScanner cs;
+  private final LeagueRepository leagueRepository;
+  //    CountryRepository countryRepository;
+
+  @Override
+  public void execute() {
+
+    System.out.println("--> Wciśnięcie '0' powoduję powtór do menu głównego <--");
+    System.out.println("Podaj nazwę drużyny:");
+
+    String name = cs.nextLine();
+    var builder = Team.builder().name(name);
+
+    System.out.println("Podaj nawę Ligi:");
+    String leagueName = cs.nextLine();
+
+    League league = leagueRepository.findByName(leagueName);
+    if (league == null) {
+      System.out.println("Tworzysz nową lige o nazwie " + leagueName);
+      System.out.println("Z jakiego Państwa jest ta liga?");
+      String country = cs.nextLine();
+
+    } else {
+      builder.league(league);
+    }
+
+    System.out.println("Podaj nawę miasta:");
+    String city = cs.nextLine();
+    builder.city(city);
+
+    System.out.println("Podaj wartość drużyny");
+    double value = Double.parseDouble(cs.nextLine());
+    builder.city(city);
+  }
+}
---
+++ src/main/java/io/mbab/sda/groupproject/repository/LeagueRepository.java
@@ -0,0 +1,57 @@
+package io.mbab.sda.groupproject.repository;
+
+import io.mbab.sda.groupproject.entity.League;
+import io.mbab.sda.groupproject.entity.Team;
+import lombok.RequiredArgsConstructor;
+
+import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
+import javax.persistence.criteria.CriteriaBuilder;
+import java.util.List;
+import java.util.Optional;
+
+@RequiredArgsConstructor
+public class LeagueRepository implements CrudRepository<Team, Integer> {
+
+    private final  EntityManager em;
+    private final Class<League> entityClass;
+
+    public League findByName(String name){
+
+            CriteriaBuilder criteriaBuilder = em.getCriteriaBuilder();
+            var criteriaQuery = criteriaBuilder.createQuery(entityClass);
+            var root = criteriaQuery.from(entityClass);
+            var entity =
+                    em.createQuery(
+                            criteriaQuery.select(root).where(criteriaBuilder.equal(root.get("name"), name)))
+                            .getSingleResult();
+
+
+         return entity;
+    }
+
+    @Override
+    public List<Team> getAll() {
+        return null;
+    }
+
+    @Override
+    public Team findById(Integer integer) {
+        return null;
+    }
+
+    @Override
+    public Team create(Team entity) {
+        return null;
+    }
+
+    @Override
+    public Team update(Team entity) {
+        return null;
+    }
+
+    @Override
+    public void delete(Integer integer) {
+
+    }
+}

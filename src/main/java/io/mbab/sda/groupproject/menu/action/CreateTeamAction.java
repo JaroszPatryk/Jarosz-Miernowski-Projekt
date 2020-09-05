02399bc4b528fe5ef01347850867634d61375efd
Uzytkownik <uzytkownik@kursGIT.pl>
2020-09-05 13:27:48 +0200

    create CreateTeamAction and LeagueRepository, and waiting for Countryrepository

--- src/main/java/io/mbab/sda/groupproject/menu/action/CreateTeamAction.java
+++ src/main/java/io/mbab/sda/groupproject/menu/action/CreateTeamAction.java
@@ -2,19 +2,16 @@ package io.mbab.sda.groupproject.menu.action;
 
 import io.mbab.sda.groupproject.entity.League;
 import io.mbab.sda.groupproject.entity.Team;
+import io.mbab.sda.groupproject.menu.CustomScanner;
 import io.mbab.sda.groupproject.repository.LeagueRepository;
 import lombok.RequiredArgsConstructor;
 
 import javax.persistence.EntityManager;
-import java.time.LocalDate;
-import java.util.List;
-import java.util.Optional;
-import java.util.Scanner;
+
 @RequiredArgsConstructor
 public class CreateTeamAction implements MenuAction {
 
-    Scanner sc;
-    private final EntityManager em;
+    CustomScanner cs;
     LeagueRepository leagueRepository;
 //    CountryRepository countryRepository;
 
@@ -25,31 +22,31 @@ public class CreateTeamAction implements MenuAction {
     System.out.println("--> Wciśnięcie '0' powoduję powtór do menu głównego <--");
     System.out.println("Podaj nazwę drużyny:");
 
-        sc = new Scanner(System.in);
-        String name = sc.nextLine();
+
+        String name = cs.nextLine();
         var builder = Team.builder().name(name);
 
         System.out.println("Podaj nawę Ligi:");
-        String leagueName = sc.nextLine();
+        String leagueName = cs.nextLine();
 
         League league = leagueRepository.findByName(leagueName);
         if(league == null){
       System.out.println("Tworzysz nową lige o nazwie " + leagueName);
       System.out.println("Z jakiego Państwa jest ta liga?");
-      String country = sc.nextLine();
+      String country = cs.nextLine();
+
 
         }else {
             builder.league(league);
         }
 
     System.out.println("Podaj nawę miasta:");
-    String city = sc.nextLine();
+    String city = cs.nextLine();
     builder.city(city);
 
     System.out.println("Podaj wartość drużyny");
-    Double value = sc.nextDouble();
+    double value = Double.parseDouble(cs.nextLine());
     builder.city(city);
     }
-
 
 }

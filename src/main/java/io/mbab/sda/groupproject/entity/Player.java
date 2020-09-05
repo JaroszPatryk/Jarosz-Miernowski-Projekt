27cbe3ca267e2a350e0a820ddd437f7165cf286a
pjaro <pjarosz27@wp.pl>
2020-09-05 11:09:21 +0200

    Created entity Player

--- src/main/java/io/mbab/sda/groupproject/entity/Player.java
+++ src/main/java/io/mbab/sda/groupproject/entity/Player.java
@@ -1,5 +1,31 @@
 package io.mbab.sda.groupproject.entity;
 
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Builder
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
 public class Player {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(length = 64, nullable = false)
+    private String firstName;
+
+    @Column(length = 64, nullable = false)
+    private String lastName;
+
+    @Column(length = 15, nullable = false)
+    private int dateOfBirth;
+
+    @Column(length = 64, nullable = false)
+    private String country;
 
 }

30346c7c9fce3808284135bc0bf0010c35de504e
pjaro <pjarosz27@wp.pl>
2020-09-06 14:23:44 +0200

    Delete PlayereService and update PlayerRepository and teamRepository

--- src/main/java/io/mbab/sda/groupproject/service/TeamService.java
+++ src/main/java/io/mbab/sda/groupproject/service/TeamService.java
@@ -16,36 +16,41 @@ import java.util.Optional;
 @RequiredArgsConstructor
 public class TeamService {
 
-  private final CountryRepository countryRepository;
-  private final LeagueRepository leagueRepository;
-  private final TeamRepository teamRepository;
-  private final EntityManager em;
+    private final CountryRepository countryRepository;
+    private final LeagueRepository leagueRepository;
+    private final TeamRepository teamRepository;
+    private final EntityManager em;
 
     public Team saveTeam(Country country, League league, Team team){
+
         try{
             em.getTransaction().begin();
             if (country.getId() == null){
                 country = countryRepository.create(country);
             }
+
             league = league.toBuilder().country(country).build();
             if(league.getId() == null){
                 league = leagueRepository.create(league);
             }
+
             team = team.toBuilder().league(league).build();
             em.persist(team);
             em.getTransaction().commit();
+
         }catch(Exception ex){
             em.getTransaction().rollback();
         }
+
         return team;
+
     }
 
+    public Optional<Country> getCountryByName(String name){
+        return countryRepository.findByName(name);
+    }
 
-    public Optional<Country> getCountryByName(String name) {
-    return countryRepository.findByName(name);
-  }
-
-  public Optional<League> getLeagueByName(String name) {
-    return leagueRepository.findByName(name);
-  }
+    public League getLeagueByName(String name){
+        return leagueRepository.findByName(name);
+    }
 }
